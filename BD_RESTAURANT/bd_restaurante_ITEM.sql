334f82c120054885073f8f6978bd5dd55b94ca67
Filipe Silva <50850709+fsilva-c@users.noreply.github.com>
2020-03-06 15:54:59 -0300

    Update bd_restaurante_ITEM.sql

--- BD_RESTAURANT/bd_restaurante_ITEM.sql
+++ BD_RESTAURANT/bd_restaurante_ITEM.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 5.7.29, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
 -- Host: localhost    Database: bd_restaurante
 -- ------------------------------------------------------
--- Server version	5.7.29-0ubuntu0.18.04.1
+-- Server version	8.0.19
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,31 +16,31 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `ITEM`
+-- Table structure for table `item`
 --
 
-DROP TABLE IF EXISTS `ITEM`;
+DROP TABLE IF EXISTS `item`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ITEM` (
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
   `nome` varchar(80) NOT NULL,
   `preco` double NOT NULL,
-  `desc` varchar(255) DEFAULT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
   `fornecedor` varchar(18) DEFAULT NULL,
   `tp_item` varchar(45) NOT NULL,
   KEY `fornecedor_idx` (`fornecedor`),
-  CONSTRAINT `fornecedor` FOREIGN KEY (`fornecedor`) REFERENCES `FORNECEDOR` (`cnpj`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fornecedor` FOREIGN KEY (`fornecedor`) REFERENCES `fornecedor` (`cnpj`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `ITEM`
+-- Dumping data for table `item`
 --
 
-LOCK TABLES `ITEM` WRITE;
-/*!40000 ALTER TABLE `ITEM` DISABLE KEYS */;
-INSERT INTO `ITEM` VALUES ('Coca cola',3.59,NULL,'12.312.322/1321-32','1');
-/*!40000 ALTER TABLE `ITEM` ENABLE KEYS */;
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES ('Coca cola',3.59,NULL,'12.312.322/1321-32','1'),('miojo',1.59,'miojo, ovo',NULL,'2');
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -52,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-05 12:26:12
+-- Dump completed on 2020-03-06 15:53:27
